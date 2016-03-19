ee317d19ad7b7444ddafe4b1455c53e4a2ceb5e2
Krishna <krish.96@hotmail.com>
2016-03-19 19:23:31 -0400

    delay on menuscript

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Threading;
 
 public class MenuScript : MonoBehaviour {
 	
 	public void loadGame () {
+        Thread.Sleep(2500);
         SceneManager.LoadScene("scene1");
     }
 
