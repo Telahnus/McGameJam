51bffa709bb4211297498d778f75cf6fa3fe2a46
Telahnus <quan.jordan@gmail.com>
2016-03-19 16:38:04 -0400

    scene change!

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+	
+	public void loadGame () {
+        SceneManager.LoadScene("scene1");
+    }
+
+}
