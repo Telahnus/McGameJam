4530f1b20f4939072bba565616f9ee2c707fe9da
Telahnus <quan.jordan@gmail.com>
2016-03-19 15:23:43 -0400

    camera controls 100%
    just add script to main camera, and pass camera to script

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -2,29 +2,19 @@
 using System.Collections;
 
 public class CameraControls : MonoBehaviour {
-    private Vector3 pos;
-    public float zoom;
-    public float spacing;
-    public float speed;
-
     public float zoomSpeed;
     public Camera minimap;
 
-    // Use this for initialization
-    void Start () {
-        pos = transform.position;
-    }
-	
-	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.W))pos.z += spacing;
-        if (Input.GetKeyDown(KeyCode.S))pos.z -= spacing;
-        if (Input.GetKeyDown(KeyCode.A))pos.x -= spacing;
-        if (Input.GetKeyDown(KeyCode.D))pos.x += spacing;
-        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
-            minimap.orthographicSize -= Input.GetAxis("Mouse ScrollWheel")*zoomSpeed;
-       
-        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+
+        minimap.orthographicSize -= Input.GetAxis("Mouse ScrollWheel")*zoomSpeed;
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        float speed = minimap.orthographicSize;
+        transform.Translate(movement * speed * Time.deltaTime);
 
     }
 }
