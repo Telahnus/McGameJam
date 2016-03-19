6cc1f8786b8df231e05f9aefae835c52b5ed7a0d
Loufe <loufeadmin@gmail.com>
2016-03-18 23:40:43 -0400

    updated the fuck out of the state behaviour code

--- Assets/behaviour.cs
+++ Assets/behaviour.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class behaviour : MonoBehaviour {
     public int owner;
     public string destination;
-    public int ID;
+    public string thisDestination;
     public GameObject voter;
-    public int voterGarrison;
+    public int garrisonValue;
     public float spawnWait;
     public Vector3 spawnValues;
     public int localVoterCount;
@@ -22,29 +22,63 @@ public class behaviour : MonoBehaviour {
     IEnumerator SpawnWaves()
     {
         //yield return new WaitForSeconds(startWait);
-        while (owner != 0 && localVoterCount != voterGarrison)
+        while (true)
         {
-            //for (int i = 0; i < hazardCount; i++)
-            //{
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
             Quaternion spawnRotation = Quaternion.identity;
-            Instantiate(voter, spawnPosition, spawnRotation);
+            Object temp = null;
+            //check for neutral state, they shouldn't be too high
+            if (owner == 0)
+            {
+                if (localVoterCount != garrisonValue)
+                {
+                    temp = Instantiate(voter, spawnPosition, spawnRotation);
+                }
+            } else {
+                temp = Instantiate(voter, spawnPosition, spawnRotation);
+            }
+            //tempOwner = temp.owner;
+            //switch (tempOwner) {
+            // case 0:
+            // case 1:
+            // case 2:
+            // }
             yield return new WaitForSeconds(spawnWait);
-            //}
-            //yield return new WaitForSeconds(waveWait);
         }
     }
+
     // Update is called once per frame
     void Update ()
     {
         updateVotersInState();
 	}
 
+    //updates the variables containing information on voters in each state
     void updateVotersInState()
     {
-        localVoterCount = 0;
-        foreignVoterCount = 0;
         //determine how many units are in the sphere of influence of the state
         //report those numbers by changing the variable localVoterCount/foreignVoterCount
+        localVoterCount = 0;
+        foreignVoterCount = 0;
+        //if local voters all killed and all voters are from same enemy, update owner of state
+        if (localVoterCount == 0 && areAllForeignVotersTheSame() == true)
+        {
+            updateOwner();
+        }
     }
+
+    //returns a true or false depending on whether all the foreign voters in a state are of the 
+    // same owner
+    private bool areAllForeignVotersTheSame()
+    {
+        return true;
+    }
+
+    //updates the owner based on current foreign attacker
+    private void updateOwner()
+    {
+        //owner = find the owner ;
+    }
+
+
 }
