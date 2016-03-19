c362e9801d8d20fa89c1b17f6fb7da02ed03e8c8
AndreiUngur <andrei.ungur@mail.mcgill.ca>
2016-03-19 12:46:46 -0400

    collisions
    Added very basic collision handling in collisionFight

---
+++ Assets/collisionFight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionFight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "testCube") {
+			Destroy(col.gameObject);
+		}
+	}
+}
