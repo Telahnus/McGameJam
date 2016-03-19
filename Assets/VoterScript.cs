73fe1d1258bda3b06102c75bd9891f9059e7588c
Telahnus <quan.jordan@gmail.com>
2016-03-19 00:23:45 -0400

    last push of the night

---
+++ Assets/VoterScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoterScript : MonoBehaviour {
+
+    public GameObject owner;
+    public int myOwner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
