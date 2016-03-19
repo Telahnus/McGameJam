563b4b816c6bbc4f832d94d39df0dfac765ffd02
Krishna <krish.96@hotmail.com>
2016-03-19 19:50:27 -0400

    dfg

---
+++ Assets/SceneFadeInOut.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;          // Speed that the screen fades to and from black.
+
+
+    private bool sceneStarting = true;      // Whether or not the scene is still fading in.
+
+    void FadeToBlack()
+    {
+        // Lerp the colour of the texture between itself and black.
+        GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.black, fadeSpeed * Time.deltaTime);
+    }
+}
