ec13d24495c720c3642faeedb5b5d1b2dab104fa
Telahnus <quan.jordan@gmail.com>
2016-03-18 20:49:52 -0400

    making my own scene, with black jack, and hookers!

--- Assets/Scripts/GenerateUnits.cs
+++ Assets/Scripts/GenerateUnits.cs
@@ -5,10 +5,7 @@ public class GenerateUnits : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValues;
-    //public int hazardCount;
     public float spawnWait;
-    //public float startWait;
-    //public float waveWait;
 
     void Start()
     {
@@ -17,17 +14,12 @@ public class GenerateUnits : MonoBehaviour {
 
     IEnumerator SpawnWaves()
     {
-        //yield return new WaitForSeconds(startWait);
         while (true)
         {
-            //for (int i = 0; i < hazardCount; i++)
-            //{
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
-            //}
-            //yield return new WaitForSeconds(waveWait);
         }
     }
 }
