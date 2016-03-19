0b0808f978d145f173cdc950adc38f7aa3a3a7cc
Telahnus <quan.jordan@gmail.com>
2016-03-19 13:16:44 -0400

    voter and wandering

---
+++ Assets/Scripts/VoterScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoterScript : MonoBehaviour {
+
+    public GameObject curState;
+    public int myOwner;
+    public string curAction;
+    public GameObject destination;
+    public bool engaged;
+    public bool atDestination;
+    // stuff for wandering
+    public float wanderRange;
+    private Vector3 wanderTarget;
+    public float wanderSpeed;
+    private float nextActionTime = 0.0f;
+    public float wanderPeriod = 1;
+    private Vector3 move;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (atDestination)
+        {
+            if (Time.time > nextActionTime)
+            {
+                nextActionTime += wanderPeriod;
+                move = new Vector3(Random.Range(-wanderRange, wanderRange), 0, Random.Range(-wanderRange, wanderRange));
+                wanderTarget = transform.position + move;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, wanderTarget, wanderSpeed);
+        }
+	}
+}
