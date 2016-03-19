8ab1ac0038abcf87330a56aa9dfce8020d008d75
Telahnus <quan.jordan@gmail.com>
2016-03-18 20:01:16 -0400

    unit spawning

--- Assets/Scripts/GenerateUnits.cs
+++ Assets/Scripts/GenerateUnits.cs
@@ -6,8 +6,8 @@ public class GenerateUnits : MonoBehaviour {
     public GameObject hazard;
     public Vector3 spawnValues;
     //public int hazardCount;
-    //public float spawnWait;
-    public float startWait;
+    public float spawnWait;
+    //public float startWait;
     //public float waveWait;
 
     void Start()
@@ -22,7 +22,7 @@ public class GenerateUnits : MonoBehaviour {
         {
             //for (int i = 0; i < hazardCount; i++)
             //{
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
