ead4c4f28e319a3101fab6ad9e0f03c2cbf5d430
AndreiUngur <andrei.ungur@mail.mcgill.ca>
2016-03-19 19:50:10 -0400

    MainSceneChangesCylinders

--- Assets/Scripts/SelectMvt.cs
+++ Assets/Scripts/SelectMvt.cs
@@ -13,10 +13,17 @@ public class SelectMvt : MonoBehaviour {
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
                 GameObject go = hit.transform.gameObject;
-                Component halo = GetComponent("Halo");
-                halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+				Behaviour h = (Behaviour)go.GetComponent ("Halo");
+				h.enabled = !h.enabled;
+				GameObject[] movingObjects = GameObject.FindGameObjectsWithTag ("MovingPlayer");
+				for (int i = 0; i < movingObjects.Length; i++) {
+					movingObjects [i].transform.position = Vector3.MoveTowards (transform.position, hit.collider.transform.position, 0.1f * Time.deltaTime);
+				}
+				if (hit.collider.gameObject.name == "Voter") {
+					GameObject voter = hit.collider.gameObject;
+					voter.tag = "MovingPlayer";
+				}
             }
 		}
 	}
-
 }
