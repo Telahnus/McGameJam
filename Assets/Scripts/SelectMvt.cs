e6d061dab9a41c6465473f959c33e26404b0f1c7
AndreiUngur <andrei.ungur@mail.mcgill.ca>
2016-03-19 18:33:51 -0400

    HALOPROBLEMS
    Halo Problems.

---
+++ Assets/Scripts/SelectMvt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectMvt : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				Component halo = GetComponent ("Halo");
+				halo.GetType ().GetProperty ("enabled").SetValue (halo, false, null);
+			}
+		}
+	}
+
+}
