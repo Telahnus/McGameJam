e8ea3f0c83a464aaca75a6fb14741cf0d957550b
Loufe <loufeadmin@gmail.com>
2016-03-20 13:36:13 -0400

    unbroke it
    # Conflicts:
    #	Assets/Scripts/SelectMvt.cs

--- Assets/Scripts/SelectMvt.cs
+++ Assets/Scripts/SelectMvt.cs
@@ -21,7 +21,7 @@ public class SelectMvt : MonoBehaviour {
 		if (Physics.Raycast (ray, out hit) && hit.transform.gameObject.name=="hitbox") {
 			GameObject go = hit.transform.gameObject;
 			Behaviour h = (Behaviour)go.GetComponent ("Halo");
-			shutDownLights ();
+			shutDownLights();
 			h.enabled = true;
 		}
 	}
@@ -36,20 +36,14 @@ public class SelectMvt : MonoBehaviour {
 	}
 	void OnMouseUp(){
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-<<<<<<< HEAD
 		RaycastHit hitUp;
-		if (Physics.Raycast(ray, out hitUp) && hitUp.transform.gameObject.name=="hitbox") {
-			GameObject go = hitUp.transform.gameObject;
-			target = new Vector3 (hitUp.point.x, 1.0f, hitUp.point.z);
+        if (Physics.Raycast(ray, out hitUp) && hitUp.transform.gameObject.name == "hitbox")
+        {
+            GameObject go = hitUp.transform.gameObject;
+            target = new Vector3(hitUp.point.x, 1.0f, hitUp.point.z);
             movement(target);
-=======
-		RaycastHit hit;
-		if (Physics.Raycast (ray, out hit) && hit.transform.gameObject.name=="hitbox") {
-			GameObject go = hit.transform.gameObject;
-			target = new Vector3 (hit.point.x, 1.0f, hit.point.z);
-			movement (target);
->>>>>>> origin/master
-		}
+        }
+
 		count = 0;
 		shutDownLights();
 	}
@@ -64,18 +58,11 @@ public class SelectMvt : MonoBehaviour {
         }
     }
 
-	void shutDownLights(){
+	void shutDownLights() {
 		while (count < cylinders.Length) {
 			j = (Behaviour)cylinders [count].GetComponent ("Halo");
 			j.enabled = false;
 			count++;
-		}
-	}
-	void movement(Vector3 target){
-		for (int i = 0; i < lolv.Count; i++) {
-			VoterScript otherTemp = lolv [i].GetComponent<VoterScript>();
-			otherTemp.curAction = "NotChilling";
-			lolv[i].transform.position = Vector3.MoveTowards (transform.position, target, 1.0f * Time.deltaTime);
 		}
 	}
 }
