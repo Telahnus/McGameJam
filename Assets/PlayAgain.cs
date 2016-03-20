d826ee9ac3c16a7d0ccf09114120ee7803b5aef8
erick <erick@hotmail.ca>
2016-03-20 13:37:19 -0400

    Add Victory Screen

---
+++ Assets/PlayAgain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAgain : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevel("UI");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
