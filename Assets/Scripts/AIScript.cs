a3c2aed4e0285a2acd90e703e71f7cbb09d69121
Telahnus <quan.jordan@gmail.com>
2016-03-20 11:46:49 -0400

    province tag

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class AIScript : MonoBehaviour {
 
+    int myNumber;
+    int commandWait;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        StartCoroutine(runAI());
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator runAI(){
+        yield return new WaitForSeconds(commandWait);
+    }
 }
--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -2,29 +2,19 @@
 using System.Collections;
 
 public class Engine : MonoBehaviour {
+
     public GameObject statesPrefab;
     public GameObject[] states;
-    // Use this for initialization
+
     void Start() {
-        if (states == null)
-        {
-            states = GameObject.FindGameObjectsWithTag("State");
-        }
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+        states = GameObject.FindGameObjectsWithTag("Province");
     }
 
     bool isWin(GameObject[] states) {
         behaviour temp1 = states[0].GetComponent<behaviour>();
-        for (int i = 1; i < states.Length - 1; i++)
-        {
-
+        for (int i = 1; i < states.Length - 1; i++){
             behaviour temp2 = states[i].GetComponent<behaviour>();
-            if (temp1.owner != temp2.owner)
-            {
+            if (temp1.owner != temp2.owner){
                 return false;
             }
         }
