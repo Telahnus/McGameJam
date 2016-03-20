f5a79a45dd03d08da986987522c6a52f6e924a29
Loufe <loufeadmin@gmail.com>
2016-03-20 11:53:57 -0400

    changed engine's comments and changed publicity of isWin function

--- Assets/Scripts/Engine.cs
+++ Assets/Scripts/Engine.cs
@@ -6,11 +6,13 @@ public class Engine : MonoBehaviour {
     public GameObject statesPrefab;
     public GameObject[] states;
 
+    //on starting the game
     void Start() {
         states = GameObject.FindGameObjectsWithTag("Province");
     }
 
-    bool isWin(GameObject[] states) {
+    // has the game been won? returns true if yes, false if no.
+    public bool isWin(GameObject[] states) {
         behaviour temp1 = states[0].GetComponent<behaviour>();
         for (int i = 1; i < states.Length - 1; i++){
             behaviour temp2 = states[i].GetComponent<behaviour>();
