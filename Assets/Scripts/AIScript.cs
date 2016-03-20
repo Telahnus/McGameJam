7f2921e47a0b5ef6ffd75ab36e145fdb3a56722a
Loufe <loufeadmin@gmail.com>
2016-03-20 19:11:23 -0400

    forgot to commit the code i wrote for the AI

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -26,13 +26,44 @@ public class AIScript : MonoBehaviour {
         Engine tempEngineScript = tempEngine.GetComponent<Engine>();
         for (int i = 2; i < 5; i++)
         {
+            List<GameObject> statesUnderAttack = new List<GameObject>();
+
             List<GameObject> myStates = tempEngineScript.stateOf(i);
             //if the player has no states left, continue
             if (myStates.Count == 0) { continue; }
             //if the player has at least one state left
             if (myStates.Count != 0)
             {
-                //foreach state do a check if they are under attack
+                //determine if the need to defend is paramount
+                for (int j = 0; j < tempEngineScript.stateOf(i).Count; j++)
+                {
+                    behaviour temp = myStates[j].GetComponent<behaviour>();
+                    if (temp.totalVoters > temp.listOfLocallyOwnedLocalVoters().Count)
+                    {
+                        statesUnderAttack.Add(myStates[j]);
+                    }
+                }
+                //if states are under attack, set other states to send help
+                if (statesUnderAttack.Count == 1)
+                {
+                    //determine if we should attack a neighbour
+                    for (int j = 0; j < tempEngineScript.stateOf(i).Count; j++)
+                    {
+                        //send all units to state which is under attack...
+                    }
+                    continue; //moves calculated, end turn
+                }
+                if (myStates.Count > (2 * statesUnderAttack.Count))
+                {
+                    //code for those states not under attack to go help those under attack
+                    continue; //moves calculated, end turn
+                } else
+                {
+                    //GIVE UP, under too much attack
+                    continue; //moves calculated, end turn
+                }
+
+                //determine if we should attack a neighbour
                 for (int j = 0; j < tempEngineScript.stateOf(i).Count; j++)
                 {
                     behaviour temp = myStates[j].GetComponent<behaviour>();
